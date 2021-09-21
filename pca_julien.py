3b8cd201fc542512c6a62ee69486d96e3acfc63d
jmilou <julien.milli@wanadoo.fr>
2021-09-21 09:48:02 +0200

    update to make it a standalone package
    Addition of an example in the "main" of pca_julien

--- pca_julien.py
+++ pca_julien.py
@@ -2,12 +2,15 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar 19 15:24:34 2019
-
 @author: jmilli
 """
 
 from pca_imagecube import pca_imagecube
-import adiUtilities as adi
+# import adiUtilities as adi
+import scipy.ndimage.interpolation
+import numpy as np
+from scipy.stats import trim_mean
+
 
 def pca_julien(cube,angle_list,cube_ref=None,ncomp=1,radii=None,method='cor',\
                trim=0.25,verbose=False):
@@ -18,9 +21,6 @@ def pca_julien(cube,angle_list,cube_ref=None,ncomp=1,radii=None,method='cor',\
         companions.
     angle_list : array_like
         Vector with the parallactic angles.
-    fwhm: int or float or 1d array, optional
-        The the Full Width Half Maximum in pixels. It can handle a different
-        FWHM value for different wavelengths (IFS data).
     """
     
     if cube_ref is None:
@@ -31,14 +31,124 @@ def pca_julien(cube,angle_list,cube_ref=None,ncomp=1,radii=None,method='cor',\
         pca= pca_imagecube(cube_ref,method=method,verbose=verbose,radii=radii,\
                         path='.',name='tmp',header=None) 
         residuals_cube = pca.compute_residuals(datacube=cube,truncation=ncomp,save=False)
-    pca_image = adi.derotateCollapse(residuals_cube,angle_list,trim=trim)
+    pca_image = derotateCollapse(residuals_cube,angle_list,trim=trim)
     return pca_image
 
 
-#vip.pca.pca(
-#    ['cube', 'angle_list', 'cube_ref=None', 'scale_list=None', 'ncomp=1', \
-#     'ncomp2=1', "svd_mode='lapack'", 'scaling=None', "adimsdi='double'", \
-#     'mask_center_px=None', 'source_xy=None', 'delta_rot=1', 'fwhm=4', \
-#     "imlib='opencv'", "interpolation='lanczos4'", "collapse='median'", \
-#     'check_mem=True', 'crop_ifs=True', 'nproc=1', 'full_output=False', \
-#     'verbose=True', 'debug=False'])
+def collapseCube(cube,trim=0.5,mask=None):
+    """
+    Collapse a cube using the median or a trimmed mean.
+    Input:
+        - cube: a cube of images
+        - trim: The collaspe along the cube 3rd dimension is a trimmed mean. It expects
+                     a value between 0 (mean) and 0.5 (median) corresponding to the 
+                     fraction of frames to remove on both sides (lower side and upper side) before 
+                     computing the mean of the cube along the axis 0. For 
+                     trim=0.1, this removes 10% of the lowest and highest pixels
+                     along the third axis of the cube before collapsing the cube
+        - mask: a 3d binary array with True for values to be discarded when collapsing
+                the cube
+    Output:
+        - collapsed image
+    """
+    if not mask is None:
+        cube = np.copy(cube)
+        print('Applying a binary mask: True values are discarded')
+        cube[mask] = np.nan
+    if trim==0.5:
+        image = np.nanmedian(cube,axis=0)
+    elif trim==0.:
+        image =np.nanmean(cube,axis=0)
+    elif trim>0. and trim < 0.5:
+        image = trim_mean(cube, trim, axis=0)
+    else:
+        raise ValueError('The clean mean expects value between 0. (mean) \
+        and 0.5 (median), received {0:4.2f}'.format(trim))
+    return image
+
+def subtractMedian(cube,trim=0.5,mask=None):
+    """
+    Subtracts the median or clean mean of a cube (first step of ADI)
+    Input:
+        - cube: a cube of images
+        - trim: if 0.5 (default value), it subtracts the median. Otherwise expects
+                     a value between 0 (mean) and 0.5 (median) corresponding to the 
+                     fraction of frames to remove on both sides (lower side and upper side) before 
+                     computing the mean of the cube along the axis 0. For 
+                     trim=0.1, this removes 10% of the lowest and highest pixels
+                     along the third axis of the cube before collapsing the cube
+        - mask: a 3d binary array with True for bad values to be replaced by nan
+                before computing the star signal
+    Output:
+        - residual cube after subtraction
+    """
+    return cube - collapseCube(cube,trim=trim,mask=mask)
+
+def derotate(cube,parang,rotoff=0.,inplace=False):
+    """
+    Derotates a cube of images according ot the parallactic angle (parang).
+    It uses scipy.ndimage.interpolation.rotate for derotation and therefore
+    expects a cube with an odd number of rows and columns, centered on the central 
+    pixel.
+    Input:
+        - cube: a cube of images
+    Output:
+        - derotated cube
+    """
+    nbFrames=cube.shape[0]
+    if len(parang) != nbFrames:
+        raise IndexError('The cube has {0:5d} frames and the parallactic angle array only {1:5d} elements'.format(nbFrames,len(parang)))
+    if inplace:
+        for i in range(nbFrames):
+            cube[i,:,:] = scipy.ndimage.interpolation.rotate(\
+                cube[i,:,:],-(parang[i]-rotoff),reshape=False,order=1,prefilter=False)
+        return cube
+    else:            
+        cubeDerotated = np.ndarray(cube.shape)
+        for i in range(nbFrames):
+    #        cubeDerotated[i,:,:] = rot.frame_rotate(cube[i,:,:],-parang[i]+rotoff)
+            cubeDerotated[i,:,:] = scipy.ndimage.interpolation.rotate(\
+                cube[i,:,:],-(parang[i]-rotoff),reshape=False,order=1,prefilter=False)
+        return cubeDerotated
+
+def derotateCollapse(cube,parang,rotoff=0.,trim=0.5,inplace=False):
+    """
+    Derotates a cube of images according ot the parallactic angle (parang) 
+    and collapses it using the median or clean mean of a cube (first step of ADI).
+    It uses scipy.ndimage.interpolation.rotate for derotation and therefore
+    expects a cube with an odd number of rows and columns, centered on the central 
+    pixel.
+    Input:
+        - cube: a cube of images
+        - trim: The collaspe along the cube 3rd dimension is a trimmed mean. It expects
+                     a value between 0 (mean) and 0.5 (median) corresponding to the 
+                     fraction of frames to remove on both sides (lower side and upper side) before 
+                     computing the mean of the cube along the axis 0. For 
+                     trim=0.1, this removes 10% of the lowest and highest pixels
+                     along the third axis of the cube before collapsing the cube
+    Output:
+        - collapsed image after derotation
+    """
+    cubeDerotated = derotate(cube,parang,rotoff=rotoff,inplace=inplace)
+    return collapseCube(cubeDerotated,trim=trim)
+
+
+
+if __name__=='__main__':
+    # example how to use this class
+
+    from astropy.io import fits
+    from pathlib import Path
+    path_package = Path(__file__).parent
+    path_data = path_package.joinpath('data')
+    cube_science = fits.getdata(path_data.joinpath('cube_science_with_fake_planet.fits'))
+    angles = np.loadtxt(path_data.joinpath('derotation_angles_science.txt'))
+
+    pca_adi = pca_julien(cube_science,angles,method='cor',verbose=True,\
+                            ncomp=30,radii=[8,20,200])
+
+    # now we assume we have a second data cube of a reference star   
+    cube_ref = fits.getdata(path_data.joinpath('cube_reference.fits'))
+    pca_rdi = pca_julien(cube_science,angles,method='cor',verbose=True,ncomp=30,\
+                            cube_ref=cube_ref,radii=[8,20,100,200])
+ 
